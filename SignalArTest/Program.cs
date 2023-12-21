35a797dbde5c7a9d7f77aeed3f00449f0992d00c
MuhammadumarMM <botirovmuhammadumar33.gmail.com>
2023-12-21 10:36:56 +0500

    Add project files.

---
+++ SignalArTest/Program.cs
@@ -0,0 +1,26 @@
+using SignalArTest.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<TestHub>("/testhub");
+
+app.Run();
